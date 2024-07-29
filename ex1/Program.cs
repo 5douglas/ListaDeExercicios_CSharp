258f0329a993595a7b9ab8cd24f619db2ad89d45
Douglas <dev.douglasf@gmail.com>
2024-07-29 07:59:17 -0300

    os tres primeiros
    os tres primeiros exercicios da lista

---
+++ ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿float quantMin = 0, quantMax = 0, medio = 0;
+
+ Console.WriteLine("ESTOQUE MÈDIO");
+
+ Console.WriteLine("Digite a quantidade mínima: ");
+quantMin = Convert.ToSingle(Console.ReadLine());
+
+Console.WriteLine("Digite a quantidade máxima");
+quantMax = Convert.ToSingle(Console.ReadLine());
+
+medio = (quantMin + quantMax) / 2;
+
+Console.WriteLine($"Resultado: {medio}");
+
---
+++ ex2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite o valor da cotação do dólar:");
+float cotacao = Convert.ToSingle(Console.ReadLine());
+
+Console.WriteLine("Digite um valor em dólares:");
+float dolares = Convert.ToSingle(Console.ReadLine());
+
+float reais = dolares * cotacao;
+
+Console.WriteLine($"Cotação: R$ {cotacao} \nDólares: $ {dolares} \nValor em reais: R$ {reais}");
