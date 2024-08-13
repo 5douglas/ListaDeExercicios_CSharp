d551db6316e30a9595a20935e23c1a04c80aa30f
Douglas <dev.douglasf@gmail.com>
2024-08-13 14:14:27 -0300

    ex6 completed

---
+++ Ex6/Program.cs
@@ -0,0 +1,40 @@
+﻿// Ler uma temperatura em graus Celsius e apresentá-Ia convertida em graus Fahrenheit. A fórmula de conversão de 
+// temperatura a ser utilizada é F = (9 * C + 160) / 5, em que a variável F representa é a temperatura em graus 
+// Fahrenheit e a variável C representa é a temperatura em graus Celsius. 
+
+using System;
+
+namespace Ex6
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+
+            Menu();
+
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("CONVERSOR CELSIUS PARA FAHRENHEIT");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Digite em Graus Celsius:");
+            float grauC = float.Parse(Console.ReadLine());
+
+            Convertor(grauC);
+        }
+
+        static void Convertor(float grauC)
+        {
+            var convertido = (9 * grauC + 160) / 5;
+
+            Console.Clear();
+            Console.WriteLine($"Celsius: {grauC} °C \nFahrenheit: {Math.Round(convertido, 2)} °F");
+            Console.ReadKey();
+
+            Menu();
+        }
+    }
+}
