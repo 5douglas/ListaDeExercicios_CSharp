b981ba8e965f7e7fd5d2d825042a7b1981c819b9
Douglas <dev.douglasf@gmail.com>
2024-08-19 10:18:33 -0300

    Ex12 completed

---
+++ Ex12/Program.cs
@@ -0,0 +1,47 @@
+﻿// Escreva um programa que leia um número inteiro e exiba o seu módulo. 
+// O módulo de um número x é: 
+// x se x é maior ou igual a zero 
+// x * (-1) se x é menor que zero
+
+
+
+using System;
+
+namespace Ex12
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Inicio();
+        }
+
+        static void Inicio()
+        {
+            Console.Clear();
+            Console.WriteLine("DESCUBRA O MÓDULO DE UM NÚMERO INTEIRO");
+            Console.WriteLine("--------------------------------------");
+
+            Console.WriteLine("Digite um número inteiro:");
+            int numero = int.Parse(Console.ReadLine());
+
+            Calculo(numero);
+        }
+
+        static void Calculo(int numero)
+        {
+            Console.Clear();
+            Console.WriteLine("RESULTADO");
+            switch(numero){
+                case 0: Console.WriteLine($"O módulo do número {numero} é 0."); break;
+                case < 0: Console.WriteLine($"O módulo do número {numero} é {numero * -1}"); break;
+                default: Console.WriteLine($"O módulo do número {numero} é {numero}"); break;
+            }
+
+            Console.WriteLine("\n-----------------------------------------");
+            Console.WriteLine("Aperte qualquer tecla para continuar...");
+            Console.ReadKey();
+            Inicio();
+        }
+    }
+}
