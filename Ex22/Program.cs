0fe3588fa55ebc804916c013cb48e44a05dd256f
Douglas <dev.douglasf@gmail.com>
2024-08-27 16:23:32 -0300

    Ex22 completed

---
+++ Ex22/Program.cs
@@ -0,0 +1,48 @@
+﻿//  Faça um programa que leia um número. Se positivo armazene-o em A, se for negativo, em B. No final mostrar o 
+// resultado. 
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Ex22
+{
+    class ListaDeExercicio
+    {
+        public static void Main(string[] args)
+        {
+            Inicio();
+        }
+
+        private static void Inicio()
+        {
+
+            Console.Clear();
+
+            Console.WriteLine("Digite um número:");
+            float numero = float.Parse(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                float A = numero;
+                var B = "vazio";
+
+                Console.Clear();
+                Console.WriteLine("Número positivo.");
+                Console.WriteLine($"Váriavel A: {A} \nVariavel B: {B}");
+            
+            }else{
+                float B = numero;
+                var A = "vazio";
+
+                Console.Clear();
+                Console.WriteLine("Número negativo.");
+                Console.WriteLine($"Váriavel A: {A} \nVariavel B: {B}");
+            }
+
+            Console.WriteLine("\n------------------");
+            Console.WriteLine("Enter para continuar");
+            Console.ReadKey();
+            Inicio();
+        }
+    }
+}
