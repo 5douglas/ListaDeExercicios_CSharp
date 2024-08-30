2e7c6d76f036779e332256f44e11bd8214310a01
Douglas <dev.douglasf@gmail.com>
2024-08-30 15:10:20 -0300

    Ex26 completed

---
+++ Ex26/Program.cs
@@ -0,0 +1,21 @@
+﻿// Escreva um programa que exiba os números de 1 a 100 na tela em ordem decrescente.
+
+using System;
+
+namespace Ex26
+{
+    class ListaDeExercicio
+    {
+        public static void Main(string[] args){
+
+            Inicio();
+        }
+        private static void Inicio()
+        {
+            Console.Clear();
+            for(int i = 100; i > 0 ; i--){
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
